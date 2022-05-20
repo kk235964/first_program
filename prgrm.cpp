780628669d2f910584bdc262ee0c654246598f52
Kamal Ahmad <kk235964@gmail.com>
2022-05-21 01:01:08 +0530

    add this file

--- prgrm.cpp
+++ prgrm.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 int main()
 {
+    int nim;
     cout<<"Hello Kamal";
     cout<<"\nWhat is your Age..";
-    cin>>nim
+    cin>>nim;
+    cout<<"YOu have entered "<<nim;
     return 0;
 }
